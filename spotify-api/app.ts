7d859b72a0a6781351717328a60f794e6e407a71
Joshua Xiao <joshuaxiao13@gmail.com>
2022-12-25 22:22:54 -0500

    renamed my-app to client

--- spotify-api/app.ts
+++ spotify-api/app.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../my-app/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use('/', router);
 
 app.listen(PORT, () => {
