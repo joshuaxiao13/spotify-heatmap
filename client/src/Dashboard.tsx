c2106df5c0a76b571df900ab20a20aefd6710fcf
Joseph Xiao <josephxiao8@gmail.com>
2022-12-26 20:52:00 -0500

    init routing

---
+++ client/src/Dashboard.tsx
@@ -0,0 +1,11 @@
+import YearHeatmap from "./heatmap/Year";
+
+const Dashboard = () => {
+  return (
+    <>
+      <YearHeatmap />
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ client/src/ErrorPage.tsx
@@ -0,0 +1,12 @@
+const ErrorPage = (props: any) => {
+    const { error } = props;
+
+    return (
+        <div id="error-page">
+            <p>Sorry, an unexpected error has occurred.</p>
+        </div>
+    )
+}
+
+
+export default ErrorPage;
