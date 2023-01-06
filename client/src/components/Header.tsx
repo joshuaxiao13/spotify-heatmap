01d96608677f958d0a57176f3934025f6a06ae4e
Joseph Xiao <josephxiao8@gmail.com>
2023-01-06 17:17:33 -0500

    use new logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,10 @@
+import logo from '../assets/heatmap_logo.png';
+
 const Header = () => {
   return (
     <header id="header" className="sticky top-0 w-full h-20 bg-slate-800 flex" style={{ color: 'white' }}>
       <div id="logoAndName" className="h-fit w-fit my-auto mx-7 flex">
-        <img
-          id="profile-image"
-          className="w-8 h-8"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
-        />
+        <img id="profile-image" className="w-8 h-8" src={logo}></img>
         <div id="name" className="mx-7 my-auto">
           Spotify Heatmap
         </div>
