070d6c8c91400659d72c469101ea31417cefe50c
Joseph Xiao <josephxiao8@gmail.com>
2023-01-06 23:01:20 -0500

    fix sticky scroll z-axis priority issue

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,10 +2,10 @@ import logo from '../assets/heatmap_logo.png';
 
 const Header = () => {
   return (
-    <header id="header" className="sticky top-0 w-full h-20 bg-slate-800 flex" style={{ color: 'white' }}>
+    <header id="header" className="sticky top-0 z-10 w-full h-20 bg-slate-800 flex">
       <div id="logoAndName" className="h-fit w-fit my-auto mx-7 flex">
         <img id="profile-image" className="w-8 h-8" src={logo}></img>
-        <div id="name" className="mx-7 my-auto">
+        <div id="name" className="mx-7 my-auto text-white">
           Spotify Heatmap
         </div>
       </div>
