27d90da582a682397825a8d64cc7f6d03e8e667e
Joseph Xiao <55932881+josephxiao8@users.noreply.github.com>
2023-01-09 18:09:42 -0500

    Example heatmap on landing (#20)
    * error page restyle, better variable naming
    
    * create funciton to generate example user listening data on landing page
    
    * fix modal message
    
    * add data for duration_ms
    
    * finish comment
    
    * click on title or logo goes back to landing
    
    * add spacing for hover comp when rendering

--- client/src/ErrorPage.tsx
+++ client/src/ErrorPage.tsx
@@ -1,12 +1,16 @@
+import Header from './components/Header';
+
 const ErrorPage = (props: any) => {
-    const { error } = props;
+  const { error } = props;
 
-    return (
-        <div id="error-page">
-            <p>Sorry, an unexpected error has occurred.</p>
-        </div>
-    )
-}
-
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex justify-center items-center flex-grow">
+        <p className="m-auto w-fit h-fit text-2xl">Sorry, an unexpected error has occurred 😕</p>
+      </div>
+    </div>
+  );
+};
 
 export default ErrorPage;
