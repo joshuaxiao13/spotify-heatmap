a42d66c274b72b32d67ca6dd57f99e4de94c8322
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2023-01-09 17:03:57 -0500

    added hover text to artist pictures and fixed placeholder positioning (#19)

--- client/src/icons/ArtistPlaceHolder.tsx
+++ client/src/icons/ArtistPlaceHolder.tsx
@@ -1,9 +1,13 @@
-export const ArtistPlaceHolder = () => {
+interface ArtistPlaceHolderProps {
+  id: string;
+}
+export const ArtistPlaceHolder = ({ id }: ArtistPlaceHolderProps) => {
   return (
     <svg
-      className="absolute w-10 h-10 text-gray-400 -left-1"
+      id={id}
+      className="w-10 h-10 text-gray-400 -left-1 bg-slate-200 rounded-full"
       fill="currentColor"
-      viewBox="0 0 20 20"
+      viewBox="0 0 20 15"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
