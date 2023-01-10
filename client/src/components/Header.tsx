52832a4280474fcc643d14762baa077d3a679996
Joshua Xiao <joshuaxiao13@gmail.com>
2023-01-10 16:18:39 -0500

    removed space in between classnames

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = (props: HeaderProps) => {
   }, []);
   const { deleteUserHandler, showModal } = props;
   return (
-    <div id="header" className="sticky  top-0 z-10 w-full h-20 bg-slate-700  flex justify-between">
+    <div id="header" className="sticky  top-0 z-10 w-full h-20 bg-slate-700 flex justify-between">
       <div id="logoAndName" className="h-fit w-fit my-auto mx-7 flex">
         <a href="./">
           <img id="profile-image" className="w-8 h-8" src={logo}></img>
